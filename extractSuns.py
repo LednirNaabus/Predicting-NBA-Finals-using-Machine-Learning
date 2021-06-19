47ef26329524844289400b35d8deff0db336faec
Rindel Subaan <valsubaan@yahoo.com>
2021-06-19 21:44:27 +0800

    markdown edit

--- extractSuns.py
+++ extractSuns.py
@@ -1,2 +1,10 @@
 from bs4 import BeautifulSoup as bs, Comment
 from pip._vendor import requests as rq
+
+def extractSuns():
+	perGameHashMap = {}
+	websiteURL = 'https://www.basketball-reference.com/playoffs/2021-nba-western-conference-semifinals-nuggets-vs-suns.html'
+	pageInstance = rq.get(websiteURL)
+
+	soup = bs(pageInstance.content, 'html.parser')
+	
